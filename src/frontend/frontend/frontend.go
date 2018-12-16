c8e94a469c25eb589b7802e0278368f44f42417c
Daniil Lemenkov <lemenkov-at-rambler-dot-ru>
2018-12-16 21:54:21 +0300

    Implement 'frontend' functions. Complete solution
    * Implement base functions in 'frontend' package:
      - 'New',
      - 'Put',
      - 'Del',
      - 'Get'.
    
    * Complete the solution of the problem.
      Also, all tests have been passed:
    
        $ make test | grep -P '(?:node/|router/|frontend/|integration_test)'
        GOPATH="..." go test node/node -count=1 -v
        ok      node/node               5.003s
        GOPATH="..." go test node/node -count=1 -race -v
        ok      node/node               6.014s
        GOPATH="..." go test router/router -count=1 -v
        ok      router/router           3.174s
        GOPATH="..." go test router/router -count=1 -race -v
        ok      router/router           4.190s
        GOPATH="..." go test frontend/frontend -count=1 -v
        ok      frontend/frontend       5.057s
        GOPATH="..." go test frontend/frontend -count=1 -race -v
        ok      frontend/frontend       6.081s
        GOPATH="..." go test integration_test -count=1 -v
        ok      integration_test        124.190s

--- src/frontend/frontend/frontend.go
+++ src/frontend/frontend/frontend.go
@@ -1,6 +1,7 @@
 package frontend
 
 import (
+	"sync"
 	"time"
 
 	rclient "router/client"
@@ -38,15 +39,60 @@ type Config struct {
 
 // Frontend is a frontend service.
 type Frontend struct {
-	// TODO: implement
+	conf        Config
+	initOnce    sync.Once
+	routerNodes []storage.ServiceAddr
 }
 
 // New creates a new Frontend with a given cfg.
 //
 // New создает новый Frontend с данным cfg.
 func New(cfg Config) *Frontend {
-	// TODO: implement
-	return nil
+	return &Frontend{
+		conf: cfg,
+	}
+}
+
+func (fe *Frontend) applyPutDel(k storage.RecordID, method func(node storage.ServiceAddr) error) error {
+	nodes, err := fe.conf.RC.NodesFind(fe.conf.Router, k)
+	if err != nil {
+		return err
+	}
+
+	if len(nodes) < storage.MinRedundancy {
+		return storage.ErrNotEnoughDaemons
+	}
+
+	results := make(chan error, len(nodes))
+	for _, node := range nodes {
+		go func(node storage.ServiceAddr) {
+			results <- method(node)
+		}(node)
+	}
+
+	okCount := 0
+	errCounts := make(map[error]int)
+
+	for range nodes {
+		err := <-results
+		if err == nil {
+			okCount++
+		} else {
+			errCounts[err]++
+		}
+	}
+
+	if okCount >= storage.MinRedundancy {
+		return nil
+	}
+
+	for err, count := range errCounts {
+		if count >= storage.MinRedundancy {
+			return err
+		}
+	}
+
+	return storage.ErrQuorumNotReached
 }
 
 // Put an item to the storage if an item for the given key doesn't exist.
@@ -55,8 +101,9 @@ func New(cfg Config) *Frontend {
 // Put -- добавить запись в хранилище, если запись для данного ключа
 // не существует. Иначе вернуть ошибку.
 func (fe *Frontend) Put(k storage.RecordID, d []byte) error {
-	// TODO: implement
-	return nil
+	return fe.applyPutDel(k, func(node storage.ServiceAddr) error {
+		return fe.conf.NC.Put(node, k, d)
+	})
 }
 
 // Del an item from the storage if an item exists for the given key.
@@ -65,8 +112,9 @@ func (fe *Frontend) Put(k storage.RecordID, d []byte) error {
 // Del -- удалить запись из хранилища, если запись для данного ключа
 // существует. Иначе вернуть ошибку.
 func (fe *Frontend) Del(k storage.RecordID) error {
-	// TODO: implement
-	return nil
+	return fe.applyPutDel(k, func(node storage.ServiceAddr) error {
+		return fe.conf.NC.Del(node, k)
+	})
 }
 
 // Get an item from the storage if an item exists for the given key.
@@ -75,6 +123,54 @@ func (fe *Frontend) Del(k storage.RecordID) error {
 // Get -- получить запись из хранилища, если запись для данного ключа
 // существует. Иначе вернуть ошибку.
 func (fe *Frontend) Get(k storage.RecordID) ([]byte, error) {
-	// TODO: implement
-	return nil, nil
+	fe.initOnce.Do(func() {
+		for {
+			nodes, err := fe.conf.RC.List(fe.conf.Router)
+			if err == nil {
+				fe.routerNodes = nodes
+				break
+			}
+			time.Sleep(InitTimeout)
+		}
+	})
+
+	nodes := fe.conf.NF.NodesFind(k, fe.routerNodes)
+
+	type result struct {
+		data []byte
+		err  error
+	}
+	results := make(chan result, len(nodes))
+
+	// Make method calls asynchronously
+	for _, node := range nodes {
+		go func(node storage.ServiceAddr) {
+			data, err := fe.conf.NC.Get(node, k)
+			results <- result{data: data, err: err}
+		}(node)
+	}
+
+	// Collect and process results of requests
+	dataCounts := make(map[string]int)
+	errCounts := make(map[error]int)
+
+	for range nodes {
+		result := <-results
+
+		if result.err != nil {
+			errCounts[result.err]++
+			if errCounts[result.err] >= storage.MinRedundancy {
+				return nil, result.err
+			}
+			continue
+		}
+
+		dataKey := string(result.data)
+		dataCounts[dataKey]++
+		if dataCounts[dataKey] >= storage.MinRedundancy {
+			return result.data, nil
+		}
+	}
+
+	return nil, storage.ErrQuorumNotReached
 }
